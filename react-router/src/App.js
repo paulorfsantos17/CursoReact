e491bb72c556fb08331d8b205d68162f46becf39
Paulo Santos <paulorfsantos17@gmail.com>
2022-07-14 20:05:22 -0300

    React router configurado

--- react-router/src/App.js
+++ react-router/src/App.js
@@ -1,25 +1,26 @@
-import logo from './logo.svg';
+
 import './App.css';
+
+import {BrowserRouter, Routes, Route} from 'react-router-dom'
+
+import Home from './pages/Home/Home'
+import About from './pages/About/About';
+
+import Navbar from './components/Navbar'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <h1>React Router</h1>
+    <BrowserRouter>
+    <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+      </Routes>
+    </BrowserRouter>
     </div>
-  );
+  )
 }
 
 export default App;
---
+++ react-router/src/hooks/useFetch.js
@@ -0,0 +1,72 @@
+import {useState, useEffect} from 'react'
+
+//4- custom hooks
+
+export const useFetch = (url) => {
+  const[data, setData] = useState(null)
+  
+  const [config, setConfig] = useState(null)
+  const [method, setMethod] = useState(null)
+  const [callFetch, setCallFetch]= useState(false)
+
+  const [loading, Setloading] =  useState(false)
+
+  const [error, setError] = useState(null)
+
+  const httpConfig = (data, method) => {
+    
+
+    if(method ==="POST" || method === 'DELETE'){
+      setConfig({
+        method,
+        headers:{
+          "content-type": "application/json"
+        },
+        body: JSON.stringify(data),
+        id: data.id
+      })
+      
+      setMethod(method)
+    }
+  }
+
+  useEffect(() => {
+    const fetchData = async () => {
+      Setloading(true)
+      
+    try {
+      const res = await fetch(url)
+      const json = await res.json()
+
+      setData(json)
+    }catch(e) {
+      setError("Houve algum erro no carregamento dos dados!")
+    }
+      Setloading(false)
+    }
+
+    fetchData()
+  }, [url, callFetch])
+
+  useEffect(() => {
+      const httpRequest = async() => {
+        
+        if(method === 'POST') {
+
+          let fetchOptions = [url, config]
+          const res =  await fetch(...fetchOptions)
+          const  json = await res.json()
+          setCallFetch(json)
+        }
+        if(method === "DELETE") {
+          let fetchOptions = [`${url}/${config.id}`, config]
+          
+          const res =  await fetch(...fetchOptions)
+          const  json = await res.json()
+          setCallFetch(json)
+        }
+      }
+      httpRequest()
+    }, [config, method, url])
+    return {data , httpConfig, loading, error}
+}
